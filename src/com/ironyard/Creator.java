3818cc29b71b943ad0464c6f906c401c6e159b5b
Stephen Schools <stephen.schools>
2017-09-26 14:35:42 -0400

    creator done? go to processor

--- src/com/ironyard/Creator.java
+++ src/com/ironyard/Creator.java
@@ -1,9 +1,14 @@
 package com.ironyard;
 
 import java.util.Scanner;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Creator {
-    public void createWorkOrders() {
+    public void createWorkOrders() throws JsonProcessingException {
         Main.orderCount += 1;
         WorkOrder newOrder = new WorkOrder();
         Scanner scanner = new Scanner(System.in);
@@ -14,9 +19,22 @@ public class Creator {
         newOrder.setSenderName(scanner.nextLine());
         newOrder.setStatus(Status.INITIAL);
         newOrder.setId(Main.orderCount);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(newOrder);
+        String fileTitle = newOrder.getId() + ".json";
+
+        try {
+            File file = new File(fileTitle);
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(json);
+            fileWriter.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws JsonProcessingException {
         Creator creator = new Creator();
         creator.createWorkOrders();
     }
